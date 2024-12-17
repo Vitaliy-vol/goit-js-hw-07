d31fff1c6cd301dd09367e0213a1052c641835bb
Viyaliy Dragan <dragan.vit.vol@gmail.com>
2024-12-17 23:04:17 +0200

    Add files via upload

---
+++ js/task-6.js
@@ -0,0 +1,45 @@
+   function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+    const input = document.querySelector("#controls input");
+    const createButton = document.querySelector("[data-create]");
+    const destroyButton = document.querySelector("[data-destroy]");
+    const boxesContainer = document.querySelector("#boxes");
+
+    function createBoxes(amount) {
+      boxesContainer.innerHTML = "";
+      const boxes = [];
+      
+      for (let i = 0; i < amount; i++) {
+        const box = document.createElement("div");
+
+        const size = 30 + 10 * i;
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
+        box.style.backgroundColor = getRandomHexColor();
+
+        boxes.push(box);
+      }
+
+      boxesContainer.append(...boxes); 
+    }
+
+    function destroyBoxes() {
+      boxesContainer.innerHTML = ""; 
+    }
+
+    createButton.addEventListener("click", () => {
+      const amount = Number(input.value);
+
+      if (amount >= 1 && amount <= 100) {
+        createBoxes(amount);
+        input.value = "";
+      } else {
+        alert("Please enter a number between 1 and 100.");
+      }
+    });
+
+    destroyButton.addEventListener("click", destroyBoxes);
